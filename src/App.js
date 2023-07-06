944ee33a1ef8fba1bb73a2b991674433efb1af02
urasurasuras <uras.oran.htc@gmail.com>
2023-07-06 00:11:24 -0400

    Added google analytics tag in head, and react-ga with router tracking

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
-import './App.css';
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
-import {Layout} from "./Pages/Layout/Layout";
-import {SpatooJr} from "./Pages/SpatooJr/SpatooJr";
-import {AboutUs} from "./Pages/AboutUs/AboutUs";
-import {Endpoints} from "./Endpoints";
-import {NotFound} from "./Pages/404/404";
-import {Home} from "./Pages/Home/Home";
-import {ZenciUras} from "./Pages/ZenciUras/ZenciUras";
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Layout } from "./Pages/Layout/Layout";
+import { SpatooJr } from "./Pages/SpatooJr/SpatooJr";
+import { AboutUs } from "./Pages/AboutUs/AboutUs";
+import { Endpoints } from "./Endpoints";
+import { NotFound } from "./Pages/404/404";
+import { Home } from "./Pages/Home/Home";
+import { ZenciUras } from "./Pages/ZenciUras/ZenciUras";
+import React, { useEffect } from "react";
+import ReactGA from "react-ga";
+
+const TRACKING_ID = "G-QLK1722KQJ";
+ReactGA.initialize(TRACKING_ID);
 
 function App() {
+  useEffect(() => {
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }, []);
+
   return (
-      <BrowserRouter>
-          <Routes>
-              <Route path="/" element={<Layout/>}>
-                  <Route index element={<Home/>}/>
-                  <Route exact path={Endpoints.Spatoo} element={<SpatooJr/>}/>
-                  <Route exact path={Endpoints.AboutUs} element={<AboutUs/>}/>
-                  <Route exact path="zenciuras" element={<ZenciUras/>}/>
-                  <Route path="*" element={<NotFound/>}/>
-              </Route>
-          </Routes>
-      </BrowserRouter>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route exact path={Endpoints.Spatoo} element={<SpatooJr />} />
+          <Route exact path={Endpoints.AboutUs} element={<AboutUs />} />
+          <Route exact path="zenciuras" element={<ZenciUras />} />
+          <Route path="*" element={<NotFound />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
